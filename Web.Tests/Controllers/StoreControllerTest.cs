d6c6c6093f400f85e16a141b229465d29751dc4a
Roman Chernigovskikh <rchernigovskix@rambler.ru>
2017-04-05 03:54:22 +0300

    Unit test commit
    Added unit tests

---
+++ Web.Tests/Controllers/StoreControllerTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Web.Controllers;
+using System.Web.Mvc;
+
+namespace Web.Tests.Controllers
+{
+    [TestClass]
+    public class StoreControllerTest
+    {
+        [TestMethod]
+        public void IndexViewResultNotNull()
+        {
+            StoreController controller = new StoreController();
+
+            ViewResult result = controller.Index() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void IndexViewEqualIndexCshtml()
+        {
+            StoreController controller = new StoreController();
+
+            ViewResult result = controller.Index() as ViewResult;
+
+            Assert.AreEqual("Index", result.ViewName);
+        }
+
+        [TestMethod]
+        public void IndexStringInViewbag()
+        {
+            StoreController controller = new StoreController();
+
+            ViewResult result = controller.Index() as ViewResult;
+
+            Assert.AreEqual("Hello world!", result.ViewBag.Message);
+        }
+    }
+}
