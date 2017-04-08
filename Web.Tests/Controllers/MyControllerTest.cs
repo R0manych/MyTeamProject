c7d37787b7708ece45e9e20e09d7cf4a4c19d58e
ViacheslavDmit <Dmitrievich.Kutsigina@yandex.ru>
2017-04-08 20:46:02 +0300

    My controller commit
    Added MyController and unit test.

---
+++ Web.Tests/Controllers/MyControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Web.Controllers;
+using System.Web.Mvc;
+
+namespace Web.Tests.Controllers
+{
+    [TestClass]
+    public class MyControllerTest
+    {
+        [TestMethod]
+        public void IndexViewEqualIndexCshtml()
+        {
+            MyController controller = new MyController();
+
+            ViewResult result = controller.Index() as ViewResult;
+
+            Assert.AreEqual("Index", result.ViewName);
+        }
+
+        [TestMethod]
+        public void IndexStringInViewbag()
+        {
+            MyController controller = new MyController();
+
+            ViewResult result = controller.Index() as ViewResult;
+
+            Assert.AreEqual("My controller method", result.ViewBag.Message);
+        }
+    }
+}
---
+++ Web/Controllers/MyController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class MyController : Controller
+    {
+        // GET: My
+        public ActionResult Index()
+        {
+            ViewBag.Message = "My controller method";
+            return View("Index");
+        }
+    }
+}
